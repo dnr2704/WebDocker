56aebc0052dd30d8029c3d622b75fe8baed38be0
Darlan <dnr2704@gmail.com>
2024-04-13 15:28:33 -0300

    adicionado login

--- webdocker/obj/Debug/net8.0/webdocker.AssemblyInfo.cs
+++ webdocker/obj/Debug/net8.0/webdocker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webdocker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e42b01c93134518a3b0e05e09c5e061524a2b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75986f40e2770b1057e06c20f3dafa3fe343c330")]
 [assembly: System.Reflection.AssemblyProductAttribute("webdocker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webdocker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
